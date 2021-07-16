7ad88c928802f1dad2e1ac1fadd4dc82d22d7a1b
kaue fraga <lolizinhafbi1234.gmail.com>
2021-07-16 10:25:17 -0300

    cache-control

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,17 @@
-function tempo(req, res) {
+async function tempo(req, res) {
+    const apiSecret = process.env.CONVERT_API_SECRET;
     const dynamicDate = new Date();
-    
+
+    const subsAPI = await fetch(`https://www.google.com?api_secret=${apiSecret}`);
+    const subsAPIJson = await subsAPI.json();
+    const subs = subsAPIJson.total_subscribers;
+
+    res.setHeader('Cache-Control', 's-manage=10, stale-while-revalidate');
+
     res.json ({
-        date: dynamicDate.toGMTString()
-    })
+        date: dynamicDate.toGMTString(),
+        subs: subs
+    });
 }
 
 export default tempo;
